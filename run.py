13de763ad7a0a6318141545f7902ea6b3f594db5
Frederic Gessler <frederic.gessler@epfl.ch>
2019-10-31 21:41:39 +0100

    Get weirdly close to good approximation with mysterious factor; need to inquire aggregation effect

--- run.py
+++ run.py
@@ -5,18 +5,19 @@ import torch
 import torch.distributed as dist
 from torch.multiprocessing import Process
 import cProfile
-size = 2
+size = 20
+
+tensor = (torch.rand(4) - 0.5) * 1000
 
 """ All-Reduce example."""
 def run(rank, size):
     """ Simple point-to-point communication. """
     group = dist.new_group(list(range(size)))
-    tensor = torch.ones(10)
     if rank == 0:
         print(tensor * size)
-    all_reduce(tensor)
+    all_reduce_1bit(tensor)
     #dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
-    print('Rank ', rank, ' has data ', tensor)
+    print('Rank ', rank, ' has data ', tensor * size)
 
 #pkill -f run.py
 
@@ -26,39 +27,43 @@ def quantize(tensor):
     Q = tensor > 0
     return Q
 def unquantize(tensor):
-    return tensor.type(torch.FloatTensor)
+    tensor = tensor.type(torch.FloatTensor)
+    tensor[tensor == 0] = -1
+    return tensor
 
 def all_reduce_1bit(tensor):
+    N = tensor.size()
     send = tensor.clone()
     recv = tensor
     rank = dist.get_rank()
     size = dist.get_world_size()
-    send_buff = torch.zeros(send.size())
-    recv_buff = torch.zeros(send.size())
-    accum = torch.zeros(send.size())
+    send_buff = torch.zeros(N)
+    recv_buff = torch.zeros(N)
+    accum = torch.zeros(N)
     accum[:] = send[:]
-
+    R = torch.zeros(N, dtype=bool)
     left = ((rank - 1) + size) % size
     right = (rank + 1) % size
     send_buff[:] = send[:]
-    error = torch.zeros(send.size())
+    error = torch.zeros(N)
     for i in range(size - 1):
-        print('round', i)
         if i % 2 == 0:
             # Send send_buff
             G = quantize(send_buff + error)
-            send_req = dist.isend(tensor=send_buff, dst=right)
-            dist.recv(tensor=recv_buff, src=left)
-            recv_buff = unquantize(recv_buff)
-            #error = send_buff - recv_buff # send_buff or G?
+            send_req = dist.isend(tensor=G, dst=right)
+            dist.recv(tensor=R, src=left)
+            recv_buff = unquantize(R)
+            error = send_buff - recv_buff # send_buff or G?
+            #print('error={}'.format(error))
             accum[:] += recv_buff[:] # + error?
         else:
             # Send recv_buff
             G = quantize(recv_buff + error)
-            send_req = dist.isend(tensor=recv_buff, dst=right)
-            dist.recv(tensor=send_buff, src=left)
-            #send_buff = unquantize(send_buff)
+            send_req = dist.isend(tensor=G, dst=right)
+            dist.recv(tensor=R, src=left)
+            send_buff = unquantize(R)
             error = recv_buff - send_buff
+            #print('error={}'.format(error))
             accum[:] += send_buff[:]
         send_req.wait()
     recv[:] = accum[:]
