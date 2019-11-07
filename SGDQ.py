ec3db87dbf2357498321bf6baeb92e9cc2327e9b
Frederic Gessler <frederic.gessler@epfl.ch>
2019-11-07 18:17:55 +0100

    Correct recieving at all-gather step

--- SGDQ.py
+++ SGDQ.py
@@ -5,19 +5,21 @@ import torch
 import torch.distributed as dist
 from torch.multiprocessing import Process
 import cProfile
+from functools import reduce
 numberOfNodes = 4
-arraySize = 256
-tensor = torch.ones(arraySize)#(torch.rand(4) - 0.5) 
+arraySize = 4
+tensors = [(torch.rand(arraySize) - 0.5) for i in range(numberOfNodes)]
 assert arraySize % numberOfNodes == 0
 """ All-Reduce example."""
 def run(rank, size):
     """ Simple point-to-point communication. """
     group = dist.new_group(list(range(numberOfNodes)))
+    goal = reduce(lambda x,y: x + y, tensors)
     if rank == 0:
-        print('Goal',tensor*numberOfNodes)
-    ms_allreduce(tensor, chunksize=int(arraySize/numberOfNodes))
+        print('Goal',goal)
+    ms_allreduce(tensors[rank], chunksize=int(arraySize/numberOfNodes))
     #dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
-    print('Rank ', rank, ' has data ', tensor)
+    print('Rank ', rank, ' has data ', tensors[rank])
 
 #pkill -f run.py
 
@@ -42,25 +44,26 @@ def ms_allreduce(tensor, chunksize=1):
     #"Naive all-reduce"
     for i in range(dist.get_world_size()): # K steps
         if i != r:
-            reqs += [dist.isend(tensor=tensor[i*chunksize:(i+1)*chunksize], dst=i)] # K concurrent transfers
+            reqs += [dist.isend(tensor=quantize(tensor[i*chunksize:(i+1)*chunksize]), dst=i)] # K concurrent transfers
     for i in range(dist.get_world_size()): # K steps
         if i != r:
-            recv = torch.zeros(arraySize)
+            recv = torch.zeros(arraySize, dtype=bool)
             dist.recv(tensor=recv[r*chunksize:(r+1)*chunksize],src=i) # K / ??? values...
-            acc += recv
+            acc += unquantize(recv)
     for req in reqs:
         req.wait()
     reqs = []
+    print(rank, 'has', acc)
     #"Naive all-gather"
     for i in range(dist.get_world_size()):
         if i != r:
-            reqs += [dist.isend(tensor=acc[r*chunksize:(r+1)*chunksize],dst=i)]
+            reqs += [dist.isend(tensor=quantize(acc[r*chunksize:(r+1)*chunksize]),dst=i)]
     #"Naive all-gather"
     for i in range(dist.get_world_size()):
         if i != r:
-            recv = torch.zeros(arraySize)
+            recv = torch.zeros(arraySize, dtype=bool)
             dist.recv(tensor=recv[i*chunksize:(i+1)*chunksize], src=i)
-            acc += recv
+            acc[i*chunksize:(i+1)*chunksize] = unquantize(recv[i*chunksize:(i+1)*chunksize])
     for req in reqs:
         req.wait()
     tensor[:] = acc[:]
