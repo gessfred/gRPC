e9687662e7a855f826e7699495d858ddbdec4baf
Frederic Gessler <frederic.gessler@epfl.ch>
2019-12-04 14:36:10 +0100

    Fix deployment(process-less)

--- examples/cluster.py
+++ examples/cluster.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-sys.path.append("../lib/")
+sys.path.append('../lib')
 import torch
 from torch import ones, zeros
 import os
@@ -8,9 +8,9 @@ from quantizy import quantizy
 import socket
 import torch.distributed as dist
 import json
-
+import datetime
 with open('config.json') as cfg:
-    config = json.load(cfg) 
+    config = json.load(cfg)
     master = config["master"]
     q, u = quantizy("cast")
     IP = master["ip"]#ifconfig
@@ -18,7 +18,4 @@ with open('config.json') as cfg:
     os.environ['MASTER_ADDR'] = IP
     os.environ['MASTER_PORT'] = '29500'
     os.environ['GLOO_SOCKET_IFNAME'] = master["interface"]
-    rank = 0 if socket.gethostbyname(socket.gethostname()) == IP else 1
-    dist.init_process_group('gloo', rank=rank, world_size=2, init_method='tcp://{}:23456'.format(IP))
-    print('connected to {} nodes'.format(dist.get_world_size()))
-print(u(q(ones(1024))))
+    dist.init_process_group('gloo', rank=os.environ['RANK'], timeout=datetime.timedelta(seconds=10), world_size=2, init_method='tcp://{}:60000'.format(IP))
