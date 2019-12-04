5d2a63d9b31b8de81e768f9cdddff9f60591a20d
Frederic Gessler <frederic.gessler@epfl.ch>
2019-12-04 14:43:03 +0100

    Import all-reduce as module

--- examples/cluster.py
+++ examples/cluster.py
@@ -4,7 +4,8 @@ sys.path.append('../lib')
 import torch
 from torch import ones, zeros
 import os
-from quantizy import quantizy
+from lib.quantizy import quantizy
+from lib.all_reduce import ms_allreduce
 import socket
 import torch.distributed as dist
 import json
@@ -12,10 +13,11 @@ import datetime
 with open('config.json') as cfg:
     config = json.load(cfg)
     master = config["master"]
-    q, u = quantizy("cast")
     IP = master["ip"]#ifconfig
     print(IP)
     os.environ['MASTER_ADDR'] = IP
     os.environ['MASTER_PORT'] = '29500'
     os.environ['GLOO_SOCKET_IFNAME'] = master["interface"]
     dist.init_process_group('gloo', rank=os.environ['RANK'], timeout=datetime.timedelta(seconds=10), world_size=2, init_method='tcp://{}:60000'.format(IP))
+    dist.new_group(2)
+    print(ms_allreduce(torch.ones(1024), *quantizy("cast")))
