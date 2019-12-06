df284738292b711cc64f6f0e62c6555054f415c8
root <root@iccluster028.iccluster.epfl.ch>
2019-12-06 14:57:51 +0100

    Improve output of benchmark script

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -16,16 +16,16 @@ def profile(iters, size, version):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-it', type=int, dest='iterations', action='store', nargs=1,
+    parser.add_argument('-it', type=int, dest='iterations', action='store',
                         default=10,
                         help='number of iterations')
-    parser.add_argument('-v', dest='version', default='cast', action='store', nargs=1, help='implementation of quantize among (cast, concept, numpy, extension)')
-    parser.add_argument('-sz', type=int, dest='size', default=12, action='store', nargs=1, help='size of the input tensor')
+    parser.add_argument('-v', dest='version', default='cast', action='store', help='implementation of quantize among (cast, concept, numpy, extension)')
+    parser.add_argument('-sz', type=int, dest='size', default=12, action='store', help='size of the input tensor')
     parser.add_argument('--flamegraph', help='store the output in a flamegraph', action='store_true')
+    parser.add_argument('-o', dest='output', default='bench', action='store', help='where to store the output file')
     args = parser.parse_args()
-    iters = args.iterations[0]
 #    profile(args.iterations[0], args.size[0], args.version[0])
-    p = Process(target=profile, args=(args.iterations[0], args.size[0], args.version[0]))
+    p = Process(target=profile, args=(args.iterations, args.size, args.version))
     p.start()
    #time.sleep(1)
     #cmd = '../../pyflame/src/pyflame'# -pid {}'.format(p.pid)
@@ -33,14 +33,16 @@ if __name__ == '__main__':
     #run(['perf script | ../../FlameGraph/stackcollapse-perf.pl > out.perf-folded'])
 #    run(['../../FlameGraph/flamegraph.pl out.perf-folded > fl.svg'])
 #NOTE: we put a timeout of 20s but it's whatever
-    txt = open('bench.txt', 'wb')
-    svg = open('bench.svg', 'wb')
+    txt = open('{}.txt'.format(args.output), 'wb')
+    svg = open('{}.svg'.format(args.output), 'wb')
     p1 = Popen(['pyflame', '-x', '-r 0.00001', '-s 20', '-p {}'.format(p.pid)], stdout=PIPE)
     if not args.flamegraph:
         txt.write(p1.stdout.read())
+        print('Saved text output to {}.txt'.format(args.output))
     else: 
         p2 = Popen(['/Flamegraph/flamegraph.pl'], stdin=p1.stdout, stdout=PIPE)
         svg.write(p2.stdout.read())
+        print('Saved FlameGraph svg output to {}.svg'.format(args.output))
     txt.close()
     svg.close()
     p.join()
