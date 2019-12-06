5d3f22a9bc7b6f4525c50888d521ddd24f0e6271
Frederic Gessler <frederic.gessler@epfl.ch>
2019-12-06 22:47:35 +0100

    Benchmark in batches in one unique script; add --all to flags

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -14,6 +14,22 @@ def profile(iters, size, version):
     for _ in range(iters):
         unqu(qu(tensor))
 
+def run(iterations, size, version, output, flamegraph):
+    p = Process(target=profile, args=(iterations, size, version))
+    p.start()
+    #NOTE: we put a timeout of 20s but it's whatever
+    p1 = Popen(['pyflame', '-x', '-r 0.00001', '-s 60', '-p {}'.format(p.pid)], stdout=PIPE)
+    if not flamegraph:
+        with open('{}.txt'.format(output), 'wb') as txt:
+            txt.write(p1.stdout.read())
+        print('Saved text output to {}.txt'.format(output))
+    else: 
+        p2 = Popen(['/Flamegraph/flamegraph.pl'], stdin=p1.stdout, stdout=PIPE)
+        with open('{}.svg'.format(output), 'wb') as svg:
+            svg.write(p2.stdout.read())
+        print('Saved FlameGraph svg output to {}.svg'.format(output))
+    p.join()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-it', type=int, dest='iterations', action='store',
@@ -23,26 +39,13 @@ if __name__ == '__main__':
     parser.add_argument('-sz', type=int, dest='size', default=12, action='store', help='size of the input tensor')
     parser.add_argument('--flamegraph', help='store the output in a flamegraph', action='store_true')
     parser.add_argument('-o', dest='output', default='bench', action='store', help='where to store the output file')
+    parser.add_argument('--all', help='run a batch benchmark for different input sizes and algorithm versions', action='store_true')
     args = parser.parse_args()
-#    profile(args.iterations[0], args.size[0], args.version[0])
-    p = Process(target=profile, args=(args.iterations, args.size, args.version))
-    p.start()
-   #time.sleep(1)
-    #cmd = '../../pyflame/src/pyflame'# -pid {}'.format(p.pid)
-    #run(['perf', 'record',  '-g',  '-F 31750', '-p {}'.format(p.pid)])
-    #run(['perf script | ../../FlameGraph/stackcollapse-perf.pl > out.perf-folded'])
-#    run(['../../FlameGraph/flamegraph.pl out.perf-folded > fl.svg'])
-#NOTE: we put a timeout of 20s but it's whatever
-    txt = open('{}.txt'.format(args.output), 'wb')
-    svg = open('{}.svg'.format(args.output), 'wb')
-    p1 = Popen(['pyflame', '-x', '-r 0.00001', '-s 60', '-p {}'.format(p.pid)], stdout=PIPE)
-    if not args.flamegraph:
-        txt.write(p1.stdout.read())
-        print('Saved text output to {}.txt'.format(args.output))
-    else: 
-        p2 = Popen(['/Flamegraph/flamegraph.pl'], stdin=p1.stdout, stdout=PIPE)
-        svg.write(p2.stdout.read())
-        print('Saved FlameGraph svg output to {}.svg'.format(args.output))
-    txt.close()
-    svg.close()
-    p.join()
+    if args.all:
+        for size in range(8, 30, 2):
+            iters = 1000 if size < 16 else 100
+            for version in ['numpy', 'ext']:
+                run(iters, size, version, 'data-{}-{}'.format(version, size), args.flamegraph)
+    else:
+        run(args.iterations, args.size, args.version, args.output, args.flamegraph)
+ 
