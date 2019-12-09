19d1bf3c75c3cb8c9df924b6fc408538b09ad79d
Frederic Gessler <frederic.gessler@epfl.ch>
2019-12-09 15:00:27 +0100

    In benchmark: add all-reduce run function

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -3,12 +3,29 @@ import sys
 sys.path.append('../lib')
 import torch
 import argparse
+from lib.all_reduce import ms_allreduce
 from quantizy import quantizy
 from torch.multiprocessing import Process
 import time
 from subprocess import run, Popen, PIPE
+import torch.distributed as dist
+import os
+import datetime
 
-def run(iters, size, version):
+def run_allreduce(iters, size, version):
+    IP = "10.90.38.4"
+    os.environ['MASTER_ADDR'] = IP
+    os.environ['MASTER_PORT'] = '29500'
+    os.environ['GLOO_SOCKET_IFNAME'] = "ens786f0"
+    dist.init_process_group('gloo', rank=os.environ['RANK'], timeout=datetime.timedelta(seconds=10), world_size=2, init_method='tcp://{}:60000'.format(IP))
+    dist.new_group(range(2))
+    time.sleep(3)
+    subject = torch.ones(2**size)
+    qn = quantizy(version)
+    for _ in range(iters):
+        ms_allreduce(subject, *qn)
+
+def run_quantize(iters, size, version):
     time.sleep(3)
     tensor = torch.ones(2**size)
     qu, unqu = quantizy(version)
@@ -51,9 +68,11 @@ if __name__ == '__main__':
     parser.add_argument('-m', dest='mode', action='store', help='output type in [flamegraph, folded, txt]')
     parser.add_argument('-o', dest='output', default='bench', action='store', help='where to store the output file')
     parser.add_argument('-t', dest='tool', default='pyflame', action='store', help='profiling tool to use')
+    parser.add_argument('-f', dest='func', default='quantize', help='function to profile', action='store')
     parser.add_argument('--all', help='run a batch benchmark for different input sizes and algorithm versions', action='store_true')
     args = parser.parse_args()
     profile = tools[args.tool] if args.tool in [k for k in tools] else pyflame
+    run = run_allreduce if args.func == 'all-reduce' else run_quantize
     if args.all:
         for size in range(8, 30, 2):
             iters = 1000
