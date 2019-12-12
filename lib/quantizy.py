70823ee5bd654d5b2bd6adc36da540fdc0a98e6a
Yralec <hipigmaster@gmail.com>
2019-12-12 11:20:18 +0100

    parallel function v1

--- lib/quantizy.py
+++ lib/quantizy.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from q_cpp import quantize_shrink, unquantize_shrink
+from q_par_cpp import quantize_shrink_par, unquantize_shrink_par
 dataSz = 32
 """
 Naive functions
@@ -76,5 +77,6 @@ def quantizy(version):
         "numpy": [quantize_vector, unquantize_vector],
         "concept": [quantize_pof, unquantize_pof],
         "ext": [quantize_shrink, unquantize_shrink],
+        "ext_par": [quantize_shrink_par, unquantize_shrink_par],
     }
     return versions[version]
