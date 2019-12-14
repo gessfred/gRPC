298c42d1aaab966162900ab35ac7c634230bc1a2
Frederic Gessler <frederic.gessler@epfl.ch>
2019-12-14 07:43:26 +0100

    Add back unsaturated all-reduce

--- lib/all_reduce.py
+++ lib/all_reduce.py
@@ -64,3 +64,39 @@ def ms_allreduce(tensor, quantize, unquantize):
         req.wait()
     tensor[:] = acc[:]
 
+def ms_allreduce_un(tensor):
+    r = dist.get_rank()
+    arraySize=list(tensor.size())[0]
+    acc = torch.zeros(arraySize)
+    world = dist.get_world_size()
+    chunksize = arraySize // world
+    assert chunksize % dataSz == 0
+    acc[r*chunksize:(r+1)*chunksize] = tensor[r*chunksize:(r+1)*chunksize]
+    reqs = []
+    #"Naive all-reduce"
+    #i = 0
+    #print('actual: {} vs. expected: {}'.format(torch.zeros(int(arraySize / (chunksize * dataSz))).size(), quantize(tensor[i*chunksize:(i+1)*chunksize]).size()))
+    for i in range(world): # K steps
+        if i != r:
+            reqs += [dist.isend(tensor=(tensor[i*chunksize:(i+1)*chunksize]), dst=i)] # K concurrent transfers
+    
+    recv = torch.zeros(arraySize // (world))
+    for i in range(world): # K steps
+        if i != r:
+            dist.recv(tensor=recv,src=i) # K / ??? values...
+            acc[r*chunksize:(r+1)*chunksize] += (recv)
+    for req in reqs:
+        req.wait()
+    reqs = []
+    #"Naive all-gather"
+    for i in range(world):
+        if i != r:
+            reqs += [dist.isend(tensor=(acc[r*chunksize:(r+1)*chunksize]),dst=i)]
+    #"Naive all-gather"
+    for i in range(world):
+        if i != r:
+            dist.recv(tensor=recv, src=i)
+            acc[i*chunksize:(i+1)*chunksize] += (recv)
+    for req in reqs:
+        req.wait()
+    tensor[:] = acc[:]
