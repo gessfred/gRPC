f32eca5946501ab5ad989061f30fbd094e21f321
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-02-24 12:56:06 +0100

    Add the original mnist example to do some cuda testing

--- lib/distributed_mnist.py
+++ lib/distributed_mnist.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from __future__ import print_function
 import argparse
 import torch
@@ -6,7 +7,11 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-
+import torch.distributed as dist
+from torch.multiprocessing import Process
+import os
+import numpy as np
+from distributed_sgd import DistributedSGD
 
 class Net(nn.Module):
     def __init__(self):
@@ -32,21 +37,30 @@ class Net(nn.Module):
         output = F.log_softmax(x, dim=1)
         return output
 
-
 def train(args, model, device, train_loader, optimizer, epoch):
+    print('training...')
+    group = dist.new_group([0, 1])
     model.train()
+    gpu = torch.device('cuda')
+    rank = dist.get_rank()
+    print('first batch.:.')
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        dim = len(target) // 4
+        data = data[:dim] if rank == 0 else data[dim:]
+        target = target[:dim] if rank == 0 else target[dim:]
+        data, target = data.to(gpu), target.to(gpu)
+        print('data.to(gpu)')
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
+        #update model gradients here
         loss.backward()
+        print('step')
         optimizer.step()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-
 
 def test(args, model, device, test_loader):
     model.eval()
@@ -67,7 +81,7 @@ def test(args, model, device, test_loader):
         100. * correct / len(test_loader.dataset)))
 
 
-def main():
+def init():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
@@ -90,31 +104,35 @@ def main():
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
+    return args
+def sanity_test():
+    pass
+def main(args):
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-    if use_cuda:
-        print('using cuda...')
+
     torch.manual_seed(args.seed)
 
     device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    datapath = os.environ['DATAPATH']
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('/mnt/data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+        datasets.MNIST(datapath, train=True, download=True,
+                    transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.1307,), (0.3081,))
+                    ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('/mnt/data', train=False, transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+        datasets.MNIST(datapath, train=False, transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.1307,), (0.3081,))
+                    ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
     model = Net().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
+    #optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    optimizer = DistributedSGD(model.parameters(), lr=args.lr)#ASGD
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
@@ -124,6 +142,14 @@ def main():
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
 
+#for running locally
+def init_processes(args, fn, size=2):
+    processes=[]
+    for rank in range(size):
+        p = Process(target=main, args=(args, rank, size))
+        p.start()
+        processes.append(p)
+    return lambda: [p.join() for p in processes]
 
 if __name__ == '__main__':
-    main()
+    main(init())
