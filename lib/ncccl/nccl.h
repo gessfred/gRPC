331ce1570b51f7ff4adefa457d0b084dc7cac7e4
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-08 14:58:57 +0100

    Add nccldatatype

--- lib/ncccl/nccl.h
+++ lib/ncccl/nccl.h
@@ -306,6 +306,25 @@ struct ncclChannel {
   };
 };
 
+/* Reduction operation selector */
+typedef enum { ncclSum        = 0,
+               ncclProd       = 1,
+               ncclMax        = 2,
+               ncclMin        = 3,
+               ncclNumOps     = 4 } ncclRedOp_t;
+
+/* Data types */
+typedef enum { ncclInt8       = 0, ncclChar       = 0,
+               ncclUint8      = 1,
+               ncclInt32      = 2, ncclInt        = 2,
+               ncclUint32     = 3,
+               ncclInt64      = 4,
+               ncclUint64     = 5,
+               ncclFloat16    = 6, ncclHalf       = 6,
+               ncclFloat32    = 7, ncclFloat      = 7,
+               ncclFloat64    = 8, ncclDouble     = 8,
+               ncclNumTypes   = 9 } ncclDataType_t;
+
 struct ncclDevComm {
   int rank;
   int nRanks;
