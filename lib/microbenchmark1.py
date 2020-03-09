59c3fdac292b641b1e41e2a019dd2041cab9b513
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-09 18:18:23 +0100

    Extend timer API

--- lib/microbenchmark1.py
+++ lib/microbenchmark1.py
@@ -10,29 +10,40 @@ class Timer(object):
     def __init__(self):
         super().__init__()
         self.profile = {}
-        self.timeline = {}
+        self.timestamps = {}
+        self.events = {}
+        self.start = time.time()
+        self.elapsed_time = 0
+        self.closed = False
 
     @contextmanager
     def __call__(self, label):
         start = self.record(label+'_start')
         yield
         end = self.record(label+'_end')
-        torch.cuda.synchronize()
-        self.profile[label] = start.elapsed_time(end)
-        
+        self.events['label'] = [start, end]
+
     def record(self, label):
         event = torch.cuda.Event(enable_timing=True)
         event.record()
-        self.timeline[label] = time.monotonic()
+        self.timestamps[label] = time.monotonic()
         return event
 
     def dump(self):
-        print(self.profile)
-        print('timeline: {}'.format(self.timeline))
+        if not self.closed:
+            self.close()
+        print('--------------------timeline--------------------')
+        print('profile: {}'.format(self.profile))
+        print('events: {}'.format(self.events))
+        print('timeline: {}'.format(self.timestamps))
+        print('elapsed_time: {}'.format(self.elapsed_time))
+        print('------------------------------------------------')
 
-timer = Timer()
+    def close(self):
+        self.closed = True
+        self.elapsed_time = time.time() - self.start
 
-def allreduce(tensor, group):
+def allreduce_(timer, tensor, group):
     torch.cuda.synchronize()
     with timer('reduce'):
         rank = dist.get_rank()
@@ -44,6 +55,28 @@ def allreduce(tensor, group):
         dist.all_gather(chunks, chunk, group=group)
     torch.cuda.synchronize()
 
+def allreduce__(timer, tensor, group):
+    torch.cuda.synchronize()
+    with timer('reduce'):
+        rank = dist.get_rank()
+        chunks = list(tensor.view(dist.get_world_size(), -1))
+        for i, chunk in enumerate(chunks):
+            dist.reduce(chunk, i, op=dist.ReduceOp.SUM, group=group)
+    with timer('all_gather'):
+        chunk = chunks[rank]
+        dist.all_gather(chunks, chunk, group=group)
+    torch.cuda.synchronize()
+
+def allreduce(tensor, group):
+    torch.cuda.synchronize()
+    rank = dist.get_rank()
+    chunks = list(tensor.view(dist.get_world_size(), -1))
+    for i, chunk in enumerate(chunks):
+        dist.reduce(chunk, i, op=dist.ReduceOp.SUM, group=group)
+    chunk = chunks[rank]
+    dist.all_gather(chunks, chunk, group=group)
+    torch.cuda.synchronize()
+
 def rendezvous(rank, world_size):
     dist.init_process_group('nccl', rank=rank, timeout=datetime.timedelta(seconds=10), world_size=world_size, init_method='tcp://{}:60000'.format(os.environ['MASTER_ADDR']))
     return dist.new_group(range(world_size))
@@ -52,10 +85,13 @@ def main():
     tensor = torch.ones(2**20).cuda()
     rank = int(os.environ['RANK'])
     group = rendezvous(rank, 2)
-    start = time.time()
+    t1 = 
     with timer('all_reduce'):
         allreduce(tensor, group)
     timer.dump()
+    print('exec time: {}'.format(time.time() - start))
+    start = time.time()
+
     print('exec time: {}'.format(time.time() - start))
     print(tensor)
 
