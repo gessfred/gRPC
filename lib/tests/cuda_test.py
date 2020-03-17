4602b25a4941c615361cf0b67e00e4c07139543e
Yralec <hipigmaster@gmail.com>
2020-03-17 16:19:44 +0100

    typos and test for speed of gpu quantisation/unquantisation

--- lib/tests/cuda_test.py
+++ lib/tests/cuda_test.py
@@ -5,11 +5,11 @@ import torch
 import time
 from quantizy import quantizy
 
-def check_quantize(q, device):
+def check_quantize(q, uq, device=None):
     # Bins are not of the same size as PyTorch uses half to even rounding.
 
     # tensor size is a multiple of 32
-    tensor = torch.cat((torch.arange(-1,1+2/256,2/256, device=device), torch.zeros(31)))
+    tensor = torch.cat((torch.arange(-1,1+2/256,2/256, device=device), torch.zeros(31, device=device)))
 
     # zero is mapped to the quantisation below
     res_1bit = torch.cat(((torch.zeros(128)-1),(torch.zeros(129)+1),(torch.zeros(31)+1)))
@@ -21,7 +21,7 @@ def check_quantize(q, device):
                 ,(torch.zeros(24)+.125),(torch.zeros(17)+.25),(torch.zeros(15)+.375),(torch.zeros(17)+.5)
                 ,(torch.zeros(15)+.625),(torch.zeros(17)+.75),(torch.zeros(15)+.875),(torch.zeros(9)+1),(torch.zeros(31)+.125)))
 
-    res_8bit = tensor.clone()
+    res_8bit = tensor.clone().cpu()
     res_8bit[128] = res_8bit[129]
     for i in range(257,288):
         res_8bit[i] = res_8bit[129]
@@ -54,6 +54,21 @@ def check_quantize(q, device):
             print(unquantized[index])
             return
 
+def check_speed(q, uq, size=10, iters=1000, device=None):
+
+    tensor = torch.rand(32*2**size)*2-1
+
+    for bits in [1,2,4,8]:
+        print('bits: {}'.format(bits))
+        start = time.time()
+        for _ in range(iters):
+            quantized = q(tensor, bits, device)
+        exec_time_q = time.time() - start
+        start = time.time()
+        for _ in range(iters):
+            unquantized = uq(quantized, bits, device)
+        exec_time_uq = time.time() - start
+        print(' Quantisation: {:6.6} / Unquantisation: {:6.6}'.format(str(exec_time_q), str(exec_time_uq)))
 
 def check_cuda():
     if torch.cuda.is_available():
@@ -67,4 +82,7 @@ def check_cuda():
 if __name__ == '__main__':
     q, uq = quantizy('gpu')
     device = check_cuda()
-    check_quantize(q, device)
+    check_quantize(q, uq, device= device)
+    check_speed(q, uq, size=16, device=device)
+    print("Using CPU:")
+    check_speed(q, uq, size=16, device=None)
