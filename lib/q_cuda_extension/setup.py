b4aa44cb7b6b706fae3a6c6011b3026a1c49a75c
Yralec <hipigmaster@gmail.com>
2020-03-18 15:04:22 +0100

    removing cuda_extension folder as the gpu reduction is now in quantizy and uses only python/pyTorch

--- lib/q_cuda_extension/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
-
-setup(name='q_cuda',
-      ext_modules=[cpp_extension.CUDAExtension('q_cuda',
-                                             ['q_cuda.cu'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
