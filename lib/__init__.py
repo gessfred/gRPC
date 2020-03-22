b098a3c36648512483da08c2ac950dd50ede666d
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-22 22:13:49 +0100

    Add toi1, tof32 to Tensor.API

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,8 +2,17 @@ import torch
 from .distributed_sgd import DistributedSGD
 from .all_reduce import allreduce, allreduce_quant
 from .timer import CUDATimer
-torch.Tensor.toi1 = None
+from .quantizy import quantize_gpu, unquantize_gpu
+def toi1(self):
+    q = quantize_gpu(self, 1, torch.cuda.current_device())
+    q.bits = 1
+    return q
+
+def tof32(self):
+    unquantize_gpu(self, self.bits, torch.cuda.current_device())
+
+torch.Tensor.toi1 = toi1
 torch.Tensor.toi2 = None
 torch.Tensor.toi4 = None
 torch.Tensor.toi8 = None
-torch.Tensor.tof32 = None
+torch.Tensor.tof32 = tof32
