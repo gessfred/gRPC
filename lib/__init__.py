c85ebb84fa8eb4e7e487bd4b8e1204f586277dee
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-24 00:59:42 +0100

    Move tof32 to Pack type

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,17 +4,11 @@ from .all_reduce import allreduce, allreduce_quant
 from .timer import CUDATimer
 from .quantizy import quantize_gpu, unquantize_gpu
 def toi1(self):
-    q = quantize_gpu(self, 1, torch.cuda.current_device())
+    q = quantize_gpu(self, 1)
     q.bits = 1
     return q
-
-def tof32(self):
-    u = unquantize_gpu(self, self.bits, torch.cuda.current_device())
-    u.bits = 32
-    return u
 
 torch.Tensor.toi1 = toi1
 torch.Tensor.toi2 = None
 torch.Tensor.toi4 = None
 torch.Tensor.toi8 = None
-torch.Tensor.tof32 = tof32
