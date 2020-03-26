64f587ca99999a2c68f8aa36f150ec8197457aab
Yralec <hipigmaster@gmail.com>
2020-03-26 18:08:15 +0100

    implemented irecv_quantized and isend_quantized, verified that it is being done asynchronously. Currently spawns another thread to do it.

--- lib/communication.py
+++ lib/communication.py
@@ -1,9 +1,36 @@
 import torch
 import torch.distributed as dist
-import quantizy import quantizy
+import asyncio
+import concurrent.futures
+import threading
+import time
+from quantizy import quantizy
 from math import ceil
-
 _pack, _unpack = quantizy('gpu')
+
+class AsyncHandler:
+
+	def __init__(self, tensor, quantized, padding, bits, handler):
+		self.loop = asyncio.get_event_loop()
+		self.t = threading.Thread(target=self.run, args=(self.loop, tensor, quantized, padding, bits, handler))
+		self.t.start()
+
+	def is_completed(self):
+		return not self.t.is_alive()
+
+	def wait(self):
+		self.t.join()
+
+	def run(self, loop, tensor, quantized, padding, bits, handler):
+		print(quantized)
+		asyncio.set_event_loop(loop)
+		loop.run_until_complete(self.irun(tensor, quantized, padding, bits, handler))
+
+	async def irun(self, tensor, quantized, padding, bits, handler):
+		handler.wait()
+		tensor.copy_(_unpack(quantized, padding, bits))
+		return True
+
 
 def send(tensor, dst):
 	rank = dist.get_rank()
@@ -25,11 +52,10 @@ def recv(tensor, src):
 def recv_quantized(tensor, src, bits, padding):
 	assert(bits in [1,2,4,8])
 	quantized_size = ceil(tensor.view(-1).shape[0]/(32/bits))
-	quantized = torch.zeros(quantized_size, device=tensor.device)
+	quantized = torch.zeros(quantized_size, dtype=torch.int32, device=tensor.device)
 	private = dist.new_group([src, dist.get_rank()])
 	dist.broadcast(quantized, src, group=private)
 	tensor.copy_(_unpack(quantized, padding, bits))
-
 
 def isend(tensor, dst):
 	rank = dist.get_rank()
@@ -38,13 +64,11 @@ def isend(tensor, dst):
 
 def isend_quantized(tensor, dst, bits):
 	assert(bits in [1,2,4,8])
-	tensor_size = tensor.view(-1).shape[0]
-	padding = (32 - pad_size) % 32
-	quantized_size = ceil(tensor_size/(32/bits))
-	quantized = torch.zeros(quantized_size, device=tensor.device)
-	private = dist.new_group([src, dist.get_rank()])
-	dist.broadcast(quantized, rank, group=private, async_op=True)
-	return padding
+	rank = dist.get_rank()
+	private = dist.new_group([rank, dst])
+	quantized, padding = _pack(tensor, bits)
+	h = dist.broadcast(quantized, rank, group=private, async_op=True)
+	return h
 
 def irecv(tensor, src):
 	private = dist.new_group([src, dist.get_rank()])
@@ -53,12 +77,12 @@ def irecv(tensor, src):
 def irecv_quantized(tensor, src, bits):
 	assert(bits in [1,2,4,8])
 	tensor_size = tensor.view(-1).shape[0]
-	padding = (32 - pad_size) % 32
+	padding = (32 - tensor_size) % 32
 	quantized_size = ceil(tensor_size/(32/bits))
-	quantized = torch.zeros(quantized_size, device=tensor.device)
+	quantized = torch.zeros(quantized_size, dtype=torch.int32, device=tensor.device)
 	private = dist.new_group([src, dist.get_rank()])
-	dist.broadcast(quantized, src, group=private, async_op=True)
-	tensor.copy_(_unpack(quantized, padding, bits))
+	handler = dist.broadcast(quantized, src, group=private, async_op=True)
+	return AsyncHandler(tensor, quantized, padding, bits, handler)
 
 def all_gather(tensor_list, tensor):
 	dist.all_gather(tensor_list, tensor)
